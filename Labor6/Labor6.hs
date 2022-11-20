4e1059d7fa33163ee2732c2d76ad21db7da20fb7
Joscha Konstantin Groth <joscha.groth2@stud.hs-flensburg.de>
2022-11-20 19:15:11 +0100

    Add Labor6 - Testat

---
+++ Labor6/Labor6.hs
@@ -0,0 +1,16 @@
+module Labor6.Labor6 where
+
+-- das Programm ist fehlerhaft, da durch das voranstellen von (==) die Präzedenz des "Operators" erhöht wird und dadurch die "Klammerung" geändert wird
+-- f x y = (==) [(1.3, 'a')] x ++ y
+
+g =
+  let f :: Num a => a -> [a] -> [a]
+      f = \x y -> 1 : x : y
+   in f 3 [2.5, 4]
+
+reverseIter :: [a] -> [a]
+reverseIter l =
+  let reverseIter' :: [a] -> [a] -> [a]
+      reverseIter' [] reversed = reversed
+      reverseIter' (x : xs) reversed = reverseIter' xs (x : reversed)
+   in reverseIter' l []
